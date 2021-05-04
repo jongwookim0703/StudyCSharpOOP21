ba652da930300c26aab035bcf29246bb8515ffd2
Jongwookim0703 <lemono0@naver.com>
2021-05-04 12:10:20 +0900

    클래스 (1)

---
+++ OOPSolution/ClassTestApp/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTestApp
+{
+    class Animal
+    {
+        private int UniqueID { get; set; }
+        protected string name { get; set; }
+        public void Eat(string meal)
+        {
+            Console.WriteLine($"{this.name} 이 {meal } 을 먹습니다");         
+        }
+        public void sleep() { Console.WriteLine($"{this.name} 이 잠을 잡니다./ "); }
+    }
+}
---
+++ OOPSolution/ClassTestApp/MainApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassTestApp
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {   
+            //기본 생성자
+            Console.WriteLine("야옹이 객체 생성 ");
+            Cat kitty = new Cat();
+            kitty.Name = "키티";
+            kitty.Age = 3;
+            kitty.Color = "하얀색";
+            kitty.Meow();
+
+             Cat nero = new Cat
+            {
+                Name = "네로",
+                Age = 65,
+                Color = "Black"
+            }; 
+            
+            nero.Meow();
+
+            //파라미터를 만들어 입력할 수 있게 만들어줌
+            Cat mimi = new Cat("미미", "노랑", 3);
+            mimi.Meow();
+
+            Cat coco = new Cat("코코", "얼룩");
+            coco.Age = 2;
+            coco.Meow();
+
+            Cat noname = new Cat("은색", 10);
+            noname.Name = "야오옹이";
+            noname.Meow();
+        }
+    }
+}
