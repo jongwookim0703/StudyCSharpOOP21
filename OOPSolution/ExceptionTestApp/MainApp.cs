17eeeea071eb92e8dd25c09b922eee322b62e7e1
Jongwookim0703 <lemono0@naver.com>
2021-05-07 17:50:43 +0900

    일반화 프로그래밍, 예외처리, 대리자와 이벤트 +feat.윈폼

--- OOPSolution/ExceptionTestApp/MainApp.cs
+++ OOPSolution/ExceptionTestApp/MainApp.cs
@@ -2,7 +2,7 @@
 
 namespace ExceptionTestApp
 {
-    class Program
+    class MainApp
     {
         static void Main(string[] args)
         {   
@@ -41,6 +41,12 @@ namespace ExceptionTestApp
             catch (Exception ex)
             {
                 Console.WriteLine($" 예외발생 : {ex.Message} ");   // 위와 같이 예외발생 경우가 많으니까 이거 하나로 퉁치기
+            }
+
+            // finally 예외가 발생하든 안하든 실행됨
+            finally
+            {
+                Console.WriteLine("Finally, 언제든지 실행됨. ");
             }
 
             Console.WriteLine("프로그램 종료!"); 
